3482208099da6b73c13a69bc920d44f490a8f198
ruboon-dej <ruboon_d@yahoo.com>
2021-01-28 19:46:27 +0700

    one final push 4real

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ for p in PROVINCES:
     if len(p) > 20 :
         print("{}...".format(p[0:17]))
 
+def get_case_for_province(province):
+    x = requests.get('https://covid19.th-stat.com/api/open/cases/sum')
+    return x.json()['Province'][province]
